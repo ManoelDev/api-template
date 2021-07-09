e5712670035587a09fa0610abb27b77d5ba4ec3a
Manoel Neto <maguserv@gmail.com>
2021-07-09 01:45:12 -0300

    chore: configure babel to build

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@config': './src/config',
+        '@database': './src/database',
+        '@modules': './src/modules',
+        '@shared': './src/shared',
+      }
+    }],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': false }]
+  ],
+}
