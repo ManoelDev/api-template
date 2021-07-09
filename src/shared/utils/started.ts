0aa246a69a9b928132638f65315bf451d450164b
Manoel Neto <maguserv@gmail.com>
2021-07-09 01:56:29 -0300

    chore: edit started infos

--- src/shared/utils/started.ts
+++ src/shared/utils/started.ts
@@ -4,7 +4,15 @@ const started = (): void => {
   console.log(``);
   console.log(`Environment:\x1b[32m ${NODE_ENV} \x1b[0m`);
   console.log(`Node_Version:\x1b[32m ${NODE_VERSION} \x1b[0m`);
-  console.log(`Running on:\x1b[32m ${SERVER_URL}:${SERVER_PORT} \x1b[0m`);
+  console.log(`Running on: \x1b[32m ${SERVER_URL}:${SERVER_PORT}/api\x1b[0m`);
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`Database
+      ╠═ host: ${process.env.DB_COMPOSE_HOST || process.env.DB_HOST}
+      ╠═ port: ${process.env.DB_PORT}
+      ╚═ data: ${process.env.DB_NAME}`);
+  }
+
   console.log(``);
 };
 
